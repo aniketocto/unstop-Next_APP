24ca2ee4448502a8491db306e0a9a16b3c4c0f41
Aniket Khambal <aniketocto@gmail.com>
2025-05-28 16:34:12 +0530

    NAVBAR

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,23 +1,35 @@
-import React from 'react'
-import '../styles/navbar.css'
-import Image from 'next/image'
+import React from "react";
+import "../styles/navbar.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
-        <Image src='/images/Logo.webp' width={150} height={100} alt='Logo'/>
+    <nav className="navbar">
+      <div className="logo-container">
+        <Image
+          src="/images/Logo.webp"
+          width={150}
+          height={100}
+          className="logo"
+          alt="Unstoppable Solution LLP"
+        />
       </div>
-      <div>
-        <ul>
-          <li>
-            
-          </li>
-        </ul>
+      <div className="nav-links-container shape-container">
+      <div className="clipped-shape">
+        <span>
+          <em></em>
+        </span>
       </div>
-      <div></div>
+        <Link href="/" className="nav-links">Home</Link>
+        <Link href="/about-us" className="nav-links">About Us</Link>
+        <p className="nav-links">Portfolio</p>
+        <p className="nav-links">Career</p>
+        <p className="nav-links">Blog</p>
+      </div>
+      <div className="cta-container"></div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
